eed6c75c5f65a113a69dea6861a850741e13f593
Mr_lan <Administrator@MC860YKCSEDW9MB>
2018-08-13 15:58:39 +0800

    拉闸测试

--- Sys.web/Controllers/HomeController.cs
+++ Sys.web/Controllers/HomeController.cs
@@ -10,18 +10,33 @@ namespace MvcDemo.Controllers
 {
     public class HomeController : Controller
     {
+        //保单界面
         public ActionResult Index()
         { return View(); }
 
+        //业务查询界面
         public ActionResult PageContent()
         { return View(); }
 
+        //用户登录界面
         public ActionResult LoginInterface()
         { return View(); }
 
+        //用户登录
+        public JsonResult Validation_User(User u)
+        {
+            bool result = false;
+
+            LoginInterface loginInit = new LoginInterface();
+
+            return Json(result);
+        }
+
+        //用户注册界面
         public ActionResult LoginView()
         { return View(); }
 
+        //用户注册功能
         public JsonResult Add_User(User u)
         {
             bool result = false;
