81e76062e2f0fcf146675bb42e66ca214738b294
Mr_lan <Administrator@MC860YKCSEDW9MB>
2018-08-13 16:05:20 +0800

    还是拉闸测试

--- Sys.dal/LoginInterface.cs
+++ Sys.dal/LoginInterface.cs
@@ -9,11 +9,19 @@ namespace Sys.dal
 {
     public class LoginInterface
     {
+        //用户注册
         public bool AddUser(User user)
             {
             string sql = string.Format("insert into Tbl_acc_inf values('{0}','{1}','{2}','{3}')",user.UserId,user.UserPasswd, user.UserName,user.UserSex);
 
             return DataHelper.ExecuteNonQuery(sql) > 0 ? true : false;
         }
+
+        //用户登录
+        public int AddValidationUser(User user)
+        {
+            return 0;
+        }
+
     }
 }
